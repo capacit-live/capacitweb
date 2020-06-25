1a84b99ba12df22c1b96939bf448e14c8218b849
Bianca <biancaasmart@gmail.com>
2020-06-25 01:14:23 -0400

    Improved data tables

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: 33.7749, lng: -84.3964},
+    zoom: 16
   });
 }
 
