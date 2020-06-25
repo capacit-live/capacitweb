f821aa9638c4fbca7927116bbc842f09aa3284bc
Bianca <biancaasmart@gmail.com>
2020-06-25 00:34:02 -0400

    Replaced map with javascript mapand fixed grid format

--- map.js
+++ map.js
@@ -5,3 +5,15 @@ function initMap() {
     zoom: 8
   });
 }
+
+/******** 
+$('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
+$('#myTab a[href="#profile"]').tab('show') // Select tab by name
+$('#myTab li:first-child a').tab('show') // Select first tab
+$('#myTab li:last-child a').tab('show') // Select last tab
+$('#myTab li:nth-child(3) a').tab('show') // Select third tab
+*/
