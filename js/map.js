31d5bf87ac6f10039d34e50ff2d6edd5dedf37bf
Bianca <biancaasmart@gmail.com>
2020-07-19 02:28:18 +0900

    Documented JS code and connected one of the demo values to the API

--- js/map.js
+++ js/map.js
@@ -34,26 +34,6 @@ var colorMatch = {
   '80-100'   : 'green'
 };
 
-/** 
-<script>
-    var settings = {
-      "url": "https://ngp39vpst8.execute-api.us-east-1.amazonaws.com/beta/-map",
-      "method": "GET",
-      "timeout": 0,
-      "headers": {
-        "tokenHeader": "allow",
-    //    "Access-Control-Allow-Origin" : "*", 
-   //     "Access-Control-Allow-Credentials" : true,
-        'Content-Type': 'application/json'
-      },
-      "body": JSON.stringify({ "message": "Hello World!" })
-    };
-    
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-    });
-</script> */
-
 var buildings = [ {building:'northave'},
                   {building:'brittain'}, 
                   {building:'westvillage'},
@@ -65,17 +45,43 @@ var buildings = [ {building:'northave'},
                   {building:'mill'} ];
 
 //$( "#northave" ).html( "Test" );
-getOccupancy(buildID);
 
-function refresh(buildings) {
+/* ******    Would display the number for north ave
+var buildID = jQuery.param(buildings[0]);
+var northave = buildings[0].building;
+var occ = getOccupancy(buildID, northave);
+$('#'+northave).html(occ);
+*/
+
+// **** Displays number for brittain on map page
+var buildID1 = jQuery.param(buildings[1]);
+var brittain = buildings[1].building;
+var occ1 = getOccupancy(buildID1, brittain);
+$('#'+brittain).html(occ1);
+
+/************** Would display the number for west village 
+var buildID2 = jQuery.param(buildings[2]);
+var westvillage = buildings[2].building;
+var occ2 = getOccupancy(buildID2, westvillage);
+$('#'+westvillage).html(occ2);
+*/
+
+// Attempt at writing a loop to display numbers for all buildings, but run into issues w/ asynchronicity 
+/*
+function refresh (buildings) {
   for (var i in buildings)
     {
       var buildID = jQuery.param(buildings[i]);
-      getOccupancy(buildID);
-      //console.log(occupancy);
-      //$('#'+buildings[i].building).html(occupancy);
+      var buildSpot = buildings[i].building;
+      var occ = getOccupancy(buildID, buildSpot);
+
+      console.log(occ);
+      $('#'+buildspot).html(occ);
     };
 };
+*/
+
+// Old code from Posrman request -- probably no longer needed just reference 
 /** 
 function getOccupancy(buildID ) {
   var settings = {
@@ -97,26 +103,39 @@ function getOccupancy(buildID ) {
     return data;
   });
 }*/
-function getOccupancy(buildID ) {
+
+/**
+ * Function to make the GET request for occupancy value for given building
+ * Time is set at default 1200
+ * This method works for individual calls but is crazy slow 
+ * @param {*} buildID The ID of the building passed as an object in form {building:'name'}
+ * @param {*} buildingSpot The location in html map page to insert the number
+ */
+function getOccupancy(buildID,buildingSpot) {
+  var rtn = "";
   //  "async":"false"
   $.ajax( {
     type: "GET",
     async:false,
     url: "https://ngp39vpst8.execute-api.us-east-1.amazonaws.com/beta/-map?time=1200&" + buildID,
+    dataType: 'json',
     headers: {
       "tokenHeader": "allow",
       "Content-Type": "application/json"
     },
     timeout: 0,
     success: function(data) {
-      $('#'+buildings[i].building).html(occupancy);
+     // $('#'+buildingSpot).html(JSON.stringify(data));
       console.log(data);
+      rtn = data;
     },
     error: function() {
       alert('Error occured');
     }
   });
-}
+  return rtn;
+};;
+;
 
 
 
